a76b7030e9ab988535939223a67ed56328c0422b
Ben Thorner <ben.thorner@bt.com>
2017-09-22 10:11:23 +0100

    Add extra describe to linker spec

--- spec/util/linker_spec.js
+++ spec/util/linker_spec.js
@@ -12,51 +12,53 @@ describe('Linker', function () {
     this.subject = new Linker(this.panel, this.linkSrv)
   })
 
-  it('assigns a link scope to each dot', function () {
-    var expected = {
-      var0: { value: 'val0' }, var1: { value: 'val1' }
-    }
+  describe('call', function () {
+    it('assigns a link scope to each dot', function () {
+      var expected = {
+        var0: { value: 'val0' }, var1: { value: 'val1' }
+      }
 
-    this.subject.call([this.dot])
-    expect(this.dot.linkScope).toEqual(expected)
-  })
-
-  describe('when the panel has no links', function () {
-    it('skips assigning the scoped link', function () {
       this.subject.call([this.dot])
-      expect(this.dot.link).toEqual(undefined)
-    })
-  })
-
-  describe('when the panel link is invalid', function () {
-    beforeEach(function () {
-      this.panel.links = []
-      this.panel.linkIndex = 0
+      expect(this.dot.linkScope).toEqual(expected)
     })
 
-    it('skips assigning the scoped link', function () {
-      this.subject.call([this.dot])
-      expect(this.dot.link).toEqual(undefined)
-    })
-  })
-
-  describe('when the panel has a link', function () {
-    beforeEach(function () {
-      this.panel.links = ['linkInfo']
-      this.panel.linkIndex = 0
+    describe('when the panel has no links', function () {
+      it('skips assigning the scoped link', function () {
+        this.subject.call([this.dot])
+        expect(this.dot.link).toEqual(undefined)
+      })
     })
 
-    it('calls the linkSrv with the dot scope', function () {
-      spyOn(this.linkSrv, 'getPanelLinkAnchorInfo')
-      this.subject.call([this.dot])
-      var linkFn = this.linkSrv.getPanelLinkAnchorInfo
-      expect(linkFn).toHaveBeenCalledWith('linkInfo', this.dot.linkScope)
+    describe('when the panel link is invalid', function () {
+      beforeEach(function () {
+        this.panel.links = []
+        this.panel.linkIndex = 0
+      })
+
+      it('skips assigning the scoped link', function () {
+        this.subject.call([this.dot])
+        expect(this.dot.link).toEqual(undefined)
+      })
     })
 
-    it('assigns a scoped link to each dot', function () {
-      spyOn(this.linkSrv, 'getPanelLinkAnchorInfo').and.returnValue('link')
-      this.subject.call([this.dot])
-      expect(this.dot.link).toEqual('link')
+    describe('when the panel has a link', function () {
+      beforeEach(function () {
+        this.panel.links = ['linkInfo']
+        this.panel.linkIndex = 0
+      })
+
+      it('calls the linkSrv with the dot scope', function () {
+        spyOn(this.linkSrv, 'getPanelLinkAnchorInfo')
+        this.subject.call([this.dot])
+        var linkFn = this.linkSrv.getPanelLinkAnchorInfo
+        expect(linkFn).toHaveBeenCalledWith('linkInfo', this.dot.linkScope)
+      })
+
+      it('assigns a scoped link to each dot', function () {
+        spyOn(this.linkSrv, 'getPanelLinkAnchorInfo').and.returnValue('link')
+        this.subject.call([this.dot])
+        expect(this.dot.link).toEqual('link')
+      })
     })
   })
 })
